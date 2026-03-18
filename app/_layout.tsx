7820748872f037019df7f2ba6feb8deb075c3c0a
LowieCasteels <lowie.casteels@gmail.com>
2026-03-18 15:54:04 +0100

    Fixed warning

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,8 +5,6 @@ export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
-      <Stack.Screen name="login" />
-      <Stack.Screen name="register" />
       <Stack.Screen name="search" />
       <Stack.Screen name="add-club" />
       <Stack.Screen name="club/[id]" />
